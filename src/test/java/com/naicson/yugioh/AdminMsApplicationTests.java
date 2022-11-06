1408687334f0316722f631569048212769246ceb
a91628319 <naicson88@hotmail.com>
2022-11-06 14:49:27 -0300

    jacoco

--- src/test/java/com/naicson/yugioh/AdminMsApplicationTests.java
+++ src/test/java/com/naicson/yugioh/AdminMsApplicationTests.java
@@ -1,5 +1,7 @@
 package com.naicson.yugioh;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,5 +9,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class AdminMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		String teste = "teste";
+		
+		  assertNotNull(teste);
+	}
 
 }
